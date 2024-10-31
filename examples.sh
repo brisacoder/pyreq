c742c313e337e1e74053299aa3919bf73cc9c547
brisacoder <dev@brisa.ai>
2024-10-31 14:12:14 -0700

    Create examples.sh

---
+++ examples.sh
@@ -0,0 +1,4 @@
+# python github_releases.py https://github.com/owner/repo YYYY-MM-DD
+# python github_releases.py https://github.com/owner/repo YYYY-MM-DD -t YOUR_PERSONAL_ACCESS_TOKEN
+# python github_releases.py https://github.com/owner/repo YYYY-MM-DD -p package-name
+# python github_releases.py https://github.com/owner/repo YYYY-MM-DD -o custom_output.txt
